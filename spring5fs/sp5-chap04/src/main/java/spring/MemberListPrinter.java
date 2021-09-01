c2dc11a01324893fd08888e7bd8bb8e32922ac8b
water-case <65841586+water-case@users.noreply.github.com>
2021-09-01 21:23:31 +0900

    210901 21:23

---
+++ spring5fs/sp5-chap04/src/main/java/spring/MemberListPrinter.java
@@ -0,0 +1,30 @@
+package spring;
+
+import java.util.Collection;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+public class MemberListPrinter {
+	
+	private MemberDao memberDao;
+	private MemberPrinter printer;
+	
+	public MemberListPrinter() {
+	}
+	
+	@Autowired
+	public void setMemberDao(MemberDao memberDao) {
+		this.memberDao = memberDao;
+	}
+
+	@Autowired
+	public void setPrinter(MemberSummaryPrinter printer) {
+		this.printer = printer;
+	}
+
+
+	public void printAll() {
+		Collection<Member> members = memberDao.selectAll();
+		members.forEach(m -> printer.print(m));
+	}
+}
