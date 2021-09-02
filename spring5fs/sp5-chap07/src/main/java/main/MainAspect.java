2a786a22f6f9d11be5eade8d687e1254554def91
water-case <65841586+water-case@users.noreply.github.com>
2021-09-02 17:40:46 +0900

    210902 17:40

---
+++ spring5fs/sp5-chap07/src/main/java/main/MainAspect.java
@@ -0,0 +1,22 @@
+package main;
+
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+import chap07.RecCalculator;
+import config.AppCtx;
+
+public class MainAspect {
+
+	public static void main(String[] args) {
+
+		AnnotationConfigApplicationContext ctx = new AnnotationConfigApplicationContext(AppCtx.class);
+		
+		RecCalculator cal = ctx.getBean("calculator", RecCalculator.class);
+		long fiveFact = cal.factorial(5);
+		System.out.println("cal.factorial(5) = " + fiveFact);
+		System.out.println(cal.getClass().getName());
+		ctx.close();
+		
+	}
+
+}
